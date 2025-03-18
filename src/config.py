177f450da00a3ad974f49721687050d4a38f10e6
mawqert <mironov.darkhorse2016@yandex.ru>
2025-03-18 22:13:09 +0300

    added project structure

---
+++ src/config.py
@@ -0,0 +1,30 @@
+from functools import cached_property
+
+from pydantic import BaseModel, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Postgres(BaseModel):
+    USER: str
+    PASSWORD: str
+    HOST: str
+    PORT: int
+    DB: str
+
+    @computed_field(return_type=str)
+    @cached_property
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DB}"
+
+class Bot(BaseModel):
+    TOKEN: str
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_nested_delimiter="__",
+    )
+
+    POSTGRES: Postgres
+    BOT: Bot
+
+app_config = Config()
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import asyncio
+
+async def main():
+    pass
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('\nExit')
