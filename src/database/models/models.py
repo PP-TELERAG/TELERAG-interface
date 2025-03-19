e0e5aff953000cf46dbf4cba288c36b23e079cd0
mawqert <mironov.darkhorse2016@yandex.ru>
2025-03-19 19:18:05 +0300

    now have db session, model for users, requests imports

---
+++ src/database/models/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy.types import BigInteger, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger)
+    source_table: Mapped[str] = mapped_column(String(45))
+
+class Source(Base):
+    pass
+
+
+
---
+++ src/database/requests.py
@@ -0,0 +1,3 @@
+from src.database.db_session import db_session
+from src.database.models.models import User
+from sqlalchemy import select
